40c3fcbb71620ec574877fa5c1a0b3638356c06e
santohack <santohack238@gmail.com>
2024-03-24 05:07:48 +0530

    first commit

---
+++ src/component/Blog/BlogData.js
@@ -0,0 +1,44 @@
+import img1 from '../../assets/img/blog/11.png'
+import img2 from '../../assets/img/blog/22.png'
+import img3 from '../../assets/img/blog/33.png'
+
+
+export const BlogData = [
+    {
+        img: img1,
+        heading: "Check out the best themes of the last year",
+        comment: 24,
+        view: 20,
+        category: "Software",
+        date: "March 14th 2020",
+        para: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel mollitia eos a.Laborum reprehenderit animi magnam ipsam tenetur natus, similique earum cum, ut
+ fugit distinctio dolores ad corporis doloribus consectetur.
+ Lorem ipsum dolor sit, amet consectetur adipisicing elit.`,
+        button: "Read More"
+    },
+    {
+        img: img2,
+        heading: "Check out the best themes of the last year",
+        comment: 24,
+        view: 20,
+        category: "Software",
+        date: "March 14th 2020",
+        para: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel mollitia eos a.Laborum reprehenderit animi magnam ipsam tenetur natus, similique earum cum, ut
+ fugit distinctio dolores ad corporis doloribus consectetur.
+ Lorem ipsum dolor sit, amet consectetur adipisicing elit.`,
+        button: "Read More"
+    },
+    {
+        img: img3,
+        heading: "Check out the best themes of the last year",
+        comment: 24,
+        view: 20,
+        category: "Software",
+        date: "March 14th 2020",
+        para: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel mollitia eos a.Laborum reprehenderit animi magnam ipsam tenetur natus, similique earum cum, ut
+ fugit distinctio dolores ad corporis doloribus consectetur.
+ Lorem ipsum dolor sit.`,
+        button: "Read More"
+    }
+]
+
